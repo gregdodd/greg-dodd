eecd7b03e688b8cc2ed6519827736b01794ca73a
Greg Dodd <greg.dodd@shopify.com>
2025-02-09 21:08:50 -0500

    basic

--- app/javascript/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuButton = document.getElementById('menu-button');
-  const menu = document.getElementById('menu');
-
-  menuButton.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
-  });
-});
